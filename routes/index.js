a77809e90f2437ee5ddb36701c23ae3d89e86379
miraan <miraan@triprapp.com>
2015-04-26 16:49:57 +0100

    configured nodejs for heroku

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var sentiment = require('sentiment');
+var Twitter = require('twitter-js-client').Twitter;
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+var r1 = sentiment('Cats are the shit.');
+console.dir(r1);
+
+router.get('/markers', function(req, res, next) {
+	res.json([]);
+})
 
 module.exports = router;
