0a30469e3b502905a510ac8aba02c3000f5dfe49
miraan <miraan100@yahoo.co.uk>
2015-05-05 01:33:57 +0100

    Lots of new backend code

---
+++ scripts/locations.js
@@ -0,0 +1,14 @@
+var locations = {
+	London: {
+		latitude: 51.497287,
+		longitude: -0.130497,
+		radius: 50
+	},
+	UK: {
+		latitude: 55.378051,
+		longitude: -3.435973,
+		radius: 600
+	}
+}
+
+module.exports.locations = locations;
--- scripts/twitter.js
+++ scripts/twitter.js
@@ -1,6 +1,9 @@
 var Twitter = require('twitter');
 var database = require('../scripts/database');
 var strings = require('../scripts/strings');
+var locations = require('../scripts/locations');
+var helper = require('../scripts/helper');
+var async = require('async');
 var _ = require('underscore')._;
 
 var client = new Twitter({
@@ -22,40 +25,197 @@ var getStream = function(searchText, callback) {
 	});
 }
 
-var saveStream = function(searchText) {
-	console.log("Saving (stream: " + searchText + ")...");
-	getStream(searchText, function(error, tweetJson) {
-		if (!error) {
-			tweetJson.searchText = searchText;
-			database.saveTweet(tweetJson, function(error, tweet) {
-				if (error) {
-					console.log("Error saving tweet (stream: " + searchText + "): " + error);
-					return;
+function URLToArray(url) {
+	var request = {};
+	var pairs = url.substring(url.indexOf('?') + 1).split('&');
+	for (var i = 0; i < pairs.length; i++) {
+	var pair = pairs[i].split('=');
+	request[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+	}
+	return request;
+}
 
-				}
-
-				console.log("Saved Tweet (stream: " + searchText + ")");
-			});
+var getPage = function(searchOptions, callback) {
+	client.get('search/tweets', searchOptions, function(error, tweets, response) {
+		if (error) {
+			callback(error, null, null);
 
 		} else {
-			console.log("Error getting tweet from (stream: " + searchText + "): " + error);
+			var nextSearchOptions;
+			if (tweets.search_metadata.next_results) {
+				 nextSearchOptions = URLToArray(tweets.search_metadata.next_results);
+			}
+			callback(null, tweets.statuses, nextSearchOptions);
+
 		}
+
 	});
 }
 
-var saveStreamsForProduct = function(product) {
-	var searchStrings = strings.getSearchStringsForProduct(product);
-	_.each(searchStrings, function(searchString) {
-		saveStream(searchString);
+// callback takes params error, tweets
+var saveTweetArray = function(tweetsToSave, options, callback) {
+	var savedTweets = [];
+	async.eachSeries(tweetsToSave, function(tweet, asyncCallback) {
+		// set the extra data about the tweet
+		if (options.product) {
+			tweet.product = options.product;
+		}
+		if (options.demand) {
+			tweet.indicatesDemand = true;
+		} else {
+			tweet.indicatesDemand = false;
+		}
+
+		database.saveTweet(tweet, function(error, savedTweet) {
+			if (error) {
+				asyncCallback(error);
+				return;
+			}
+
+			savedTweets.push(savedTweet);
+			asyncCallback();
+		});
+
+	}, function(error) {
+		if (error) {
+			callback(error, null)
+			return;
+		}
+
+		callback(null, savedTweets);
 	});
 }
 
-var products = ["iphone 6", "galaxy s6", "htc one m9", "xperia z3"];
-// below code doesn't work because twitter refuses that many stream connections
-// _.each(products, function(product) {
-// 	saveStreamsForProduct(product);
-// });
+// callback takes params (error, tweets)
+var doSearch = function(searchText, options, callback) {
+	var batch = 100;
+	var searchOptions = {};
+	searchOptions.q = searchText;
+	searchOptions.lang = 'en';
+	// searchOptions.result_type = 'recent';
+	searchOptions.count = batch;
+	if (options.latitude && options.longitude && options.radius) {
+		var geocodeString = options.latitude + "," + options.longitude + "," + options.radius + "km";
+		searchOptions.geocode = geocodeString;
+	}
 
-saveStream("want lg g4");
+	// saveCallback takes params (error, tweets)
+	var saveTweetBatch = function(tweets, saveCallback) {
+		if (!options.save) {
+			saveCallback(null, []);
+			return;
+		}
 
-module.exports.getStream = getStream;
+		saveTweetArray(tweets, options, function(error, savedTweets) {
+			if (error) {
+				saveCallback(error, null);
+				return;
+			}
+
+			saveCallback(null, savedTweets);
+		});
+	};
+
+	var allTweets = [];
+
+	var getPageCallback = function(error, tweets, nextSearchOptions) {
+		if (error) {
+			callback(error, null);
+			return;
+
+		}
+
+		allTweets = allTweets.concat(tweets);
+		console.log("current search: " + searchText + ", received tweets: " + allTweets.length);
+
+		saveTweetBatch(tweets, function(error, savedTweets) {
+			if (error) {
+				callback(error, null);
+				return;
+			}
+
+			console.log("saved " + savedTweets.length + " tweets");
+
+			if (!nextSearchOptions || (options.limit && allTweets.length >= options.limit)) {
+				callback(null, allTweets);
+				return;
+			}
+
+			getPage(nextSearchOptions, getPageCallback);
+
+		});
+
+	};
+
+	getPage(searchOptions, getPageCallback);
+}
+
+// callback takes params error, tweets
+var getRandomSample = function(size, searchText, options, callback) {
+	var limit = 1000;
+	doSearch(searchText, options, function(error, tweets) {
+		if (error) {
+			callback(error, null);
+			return;
+		}
+
+		var randomTweets = helper.getRandomSample(size, tweets);
+		callback(null, randomTweets);
+	});
+}
+
+// callback has params error, tweets
+// limit is on each search
+var getTweets = function(options, callback) {
+	if (!options.product) {
+		callback("No product given", null);
+		return;
+	}
+
+	// get the searches we are performing
+	var searches;
+	if (options.demand) {
+		searches = strings.getSearchStringsForProduct(options.product);
+	} else {
+		searches = [options.product];
+	}
+
+	var allTweets = [];
+	var allSavedTweets = [];
+	// var uk = locations.locations.UK;
+	async.eachSeries(searches, function(search, asyncCallback) {
+		doSearch(search, options, function(error, tweets) {
+			if (error) {
+				asyncCallback(error);
+				return;
+			}
+
+			allTweets = allTweets.concat(tweets);
+			asyncCallback();
+		});
+
+	}, function(error) {
+		if (error) {
+			callback(error, null);
+			return;
+		}
+
+		callback(null, allTweets);
+	});
+};
+
+// callback has params error, tweets
+var saveTweets = function(options, callback) {
+	options.save = true;
+	getTweets(options, function(error, tweets) {
+		if (error) {
+			callback(error, null);
+			return;
+		}
+
+		callback(null, tweets);
+	});
+}
+
+module.exports.getRandomSample = getRandomSample;
+module.exports.saveTweets = saveTweets;
